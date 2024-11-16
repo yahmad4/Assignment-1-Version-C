ab94775f1914599bbe5b4d350ad73e2cf3a3ceec
Yasir Ahmad <yahmad4@myseneca.ca>
2024-11-16 15:20:46 -0500

    done with assignment 1

--- assignment1.py
+++ assignment1.py
@@ -11,9 +11,10 @@ with anyone or anything except for submission for grading. I understand
 that the Academic Honesty Policy will be enforced and
 violators will be reported and appropriate action will be taken.
 
-Author: <Student Name>
-Semester: <Fall/Winter/Summer> <Year>
-Description: <fill this in>
+Author: Yasir Ahmad (Yahmad4)
+Semester: Fall 2024
+Description: A Python program to calculate an end date given a start date
+and a number of days, while validating inputs and considering leap years.
 '''
 
 import sys
@@ -173,33 +174,6 @@ def day_iter(start_date: str, num: int) -> str:
 if __name__ == "__main__":
     import sys
 
-    # Check the number of arguments
-    if len(sys.argv) != 3:
-        usage()
-
-    # Parse and validate the first argument (date)
-    start_date = sys.argv[1]
-    if not valid_date(start_date):
-        usage()
-
-    # Parse and validate the second argument (number of days)
-    try:
-        num_days = int(sys.argv[2])
-    except ValueError:
-        usage()
-
-    # Calculate the end date
-    end_date = day_iter(start_date, num_days)
-
-    # Determine the day of the week for the end date
-    day_name = day_of_week(end_date)
-
-    # Print the result
-    print(f"The end date is {day_name}, {end_date}.")
-
-if __name__ == "__main__":
-    import sys
-
     # check length of arguments
     if len(sys.argv) != 3:
         usage()
@@ -221,5 +195,3 @@ if __name__ == "__main__":
     # print(f'The end date is {day_of_week(x)}, {x}.')
     day_name = day_of_week(end_date)
     print(f"The end date is {day_name}, {end_date}.")
-
-    pass
